e15178245c36fdcdd2d6aeff7990de7444ba4dd0
Enmanuel Hernandez <enmanuelh17@gmail.com>
2017-06-13 21:20:40 -0400

    create rename function

--- rename_files.py
+++ rename_files.py
@@ -1,9 +1,14 @@
 #rename files
 #E hernandez
+from os import listdir
 
-import os import listdir
-
-def rename_files()
+def rename_files():
 	#1 get file names from a folder
 	file_list = listdir(r"/home/enmanuel/Downloads/prank/prank/")
 	print (file_list)
+	#(2) for each file, rename filename
+	for file_name in file_list:
+		os.rename(file_name,file_name.translate(None, "0123456789"))
+		
+
+rename_files()
