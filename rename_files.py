1e135d9e4bd7068a70935ff74ad52b35d1d89ba7
Enmanuel Hernandez <enmanuelh17@gmail.com>
2017-06-13 21:12:29 -0400

    adding rename_files.py

---
+++ rename_files.py
@@ -0,0 +1,9 @@
+#rename files
+#E hernandez
+
+import os import listdir
+
+def rename_files()
+	#1 get file names from a folder
+	file_list = listdir(r"/home/enmanuel/Downloads/prank/prank/")
+	print (file_list)
