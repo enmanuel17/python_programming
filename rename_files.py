9cf7ce65da07107e9cb442b0604d8718d89ee023
Enmanuel Hernandez <enmanuelh17@gmail.com>
2017-06-13 21:48:20 -0400

    fixed indentation issue near os.chdir

--- rename_files.py
+++ rename_files.py
@@ -1,14 +1,19 @@
 #rename files
 #E hernandez
-from os import listdir
+import os
+
 
 def rename_files():
 	#1 get file names from a folder
-	file_list = listdir(r"/home/enmanuel/Downloads/prank/prank/")
+	file_list = os.listdir("/home/enmanuel/Downloads/prank/prank/")
 	print (file_list)
+	saved_path = os.getcwd() #gets the current dir.
+	print("Current Working Directory is"+saved_path)
+	os.chdir("/home/enmanuel/Downloads/prank/prank") #changes the current dir for this program
 	#(2) for each file, rename filename
 	for file_name in file_list:
-		os.rename(file_name,file_name.translate(None, "0123456789"))
-		
+		print (file_name)
+		os.rename(file_name, file_name.translate(None, "0123456789"))
+	os.chdir(saved_path)
 
 rename_files()
