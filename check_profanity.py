8bd13dc8f70fe99c9f8f4ece43df4deac0e973b9
Enmanuel Hernandez <enmanuelh17@gmail.com>
2017-07-16 18:06:48 -0400

    forgot a : when defining read_text

--- check_profanity.py
+++ check_profanity.py
@@ -2,7 +2,7 @@
 # Create a profanity checker, it partest a files and checks for bad words.
 
 #Defining the main function.
-def read_text()
+def read_text():
 	quotes = open("/home/enmanuel/homework/python_programming/movie_quotes.txt") #opening a file for checking and assigning it to a varialbe for later user.
 	contents_of_file = quotes.read() #reading the contents of the file into into a variable
 	print(contents_of_file) #printing the contents of the file to the screen
