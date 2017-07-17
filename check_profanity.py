fa87ad316cdb6e837c0cdd0cb5f6c52828df5d54
Enmanuel Hernandez <enmanuelh17@gmail.com>
2017-07-16 23:23:24 -0400

    Added a function to check for bad words

--- check_profanity.py
+++ check_profanity.py
@@ -1,12 +1,23 @@
 #Enmanuel Hernandez
 # Create a profanity checker, it partest a files and checks for bad words.
 
+import urllib #imports a library to manage url objects
+
 #Defining the main function.
 def read_text():
-	quotes = open("/home/enmanuel/homework/python_programming/movie_quotes.txt") #opening a file for checking and assigning it to a varialbe for later user.
-	contents_of_file = quotes.read() #reading the contents of the file into into a variable
-	print(contents_of_file) #printing the contents of the file to the screen
+	quotes = open("/home/enmanuel/homework/python_programming/movie_quotes.txt") #opening a file for checking and creating an object of it.
+	contents_of_file = quotes.read() #reading the contents of the object into into a variable
+	print(contents_of_file) #printing the contents of the object to the screen
 	quotes.close() #closes the file. it is good practice to always close a file at the end.
+	check_profanity(contents_of_file) #Calls function to check for bad words.
+
+#fuction that checks for profanity.
+def check_profanity(text_to_check):
+	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check) #open the url as an option called connection.
+	output = connection.read() #store the output of the GET from the URL inside a variable
+	print(output) 			   #print the output of the GET from the URL.
+	connection.close()			#close the url object.
+
 
 #calls the main funtion
 read_text()
